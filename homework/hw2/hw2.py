dfa53fa28b14cb5278e862c4d46488f2691bc8d8
Chandler J <cjustice2000@gmail.com>
2024-01-31 08:38:04 -0700

    hw2 progress

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -1,28 +1,59 @@
 import math
+from typing import Callable
+from scipy.sparse import diags
+import numpy as np
+
+
 
 class FiniteDifference:
     interval_points = []
+    
+    approx_solutions = []
+    function_values = []
+    matrix = np.array 
+    def __init__(self, left_endpoint: float, right_endpoint: float, num_points: int, fx: Callable) -> None:
 
-    def __init__(self, left_endpoint, right_endpoint, num_points, function):
+        self.left_endpoint = left_endpoint
         self.interval_points.append(left_endpoint)
+
+        self.right_endpoint = right_endpoint
         self.interval_points.append(right_endpoint)
-        
+
+        self.num_points = num_points 
+
         self.create_interval_points(num_points)
         
+        self.fx = fx 
         print(self.interval_points, len(self.interval_points))
 
+    def determine_mesh_interval(self):
+        return (self.right_endpoint - self.left_endpoint) / (self.num_points - 1)
 
     def create_interval_points(self, num_points):
-
-        left_e = self.interval_points[0]
-        right_e = self.interval_points[-1]
-        #self.interval_points.remove(right_e)
-
-        value_interval = (right_e - left_e) / (num_points - 1)
-        curr_val = left_e + value_interval 
+        value_interval = self.determine_mesh_interval()
+        
+        curr_val = self.left_endpoint + value_interval 
         for i in range(1, num_points - 1):
             self.interval_points.insert(i, curr_val)
             curr_val += value_interval
+        
+        # add the m+1st point
 
+        curr_val += value_interval
+        self.interval_points.append(curr_val)
 
-test = FiniteDifference(5, 20, 8, lambda x: math.sin(x))  
+    def construct_matrix(self):
+        n = 10
+        k = [np.ones(n-1),-2*np.ones(n),np.ones(n-1)]
+        offset = [-1,0,1]
+        self.matrix = diags(k,offset).toarray()
+
+    def evaluate_function(self, x):
+        return self.fx(x)
+
+def main(): 
+    test = FiniteDifference(0, 1, 8, lambda x: math.sin(x))  
+    print(test.evaluate_function(math.radians(90)))
+    test.construct_matrix()
+    print(test.matrix)
+main()
