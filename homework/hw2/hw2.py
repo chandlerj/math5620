29a0ec6fb86144a17f54bb0c66518af0e77ed0b0
Chandler J <cjustice2000@gmail.com>
2024-02-05 16:03:18 -0700

    feb 5 lect

--- homework/hw2/hw2.py
+++ homework/hw2/hw2.py
@@ -6,6 +6,8 @@ import numpy as np
 
 
 class FiniteDifference:
+
+
     def __init__(self, left_endpoint: float, right_endpoint: float, num_points: int, fx: Callable) -> None:
         # initialize data
 
@@ -30,23 +32,27 @@ class FiniteDifference:
 
         self.h = 1 / (1 + self.mesh_interval)
 
+
     def determine_mesh_interval(self):
         return (self.right_endpoint - self.left_endpoint) / (self.num_points - 1)
 
+
     def create_interval_points(self):
-        
         curr_val = self.left_endpoint + self.mesh_interval
         for i in range(1, self.num_points - 1):
             self.interval_points.insert(i, curr_val)
             curr_val += self.mesh_interval
-        
+   
+
     def construct_matrix(self):
         k = [np.ones(self.num_points-1),-2*np.ones(self.num_points),np.ones(self.num_points-1)]
         offset = [-1,0,1]
         self.matrix = diags(k,offset).toarray()
 
+
     def evaluate_function(self, x):
         return self.fx(x)
+
 
     def create_approximate_solutions(self):
         self.function_values.append(self.left_endpoint)
@@ -56,7 +62,8 @@ class FiniteDifference:
             second_difference = (1 / self.h**2) * (self.fx(self.interval_points[i - 1]) - (2 * self.fx(self.interval_points[i])) + self.fx(self.interval_points[i + 1]))
             self.function_values.append(second_difference)
         self.function_values.append(self.right_endpoint)
-   
+
+
     def solve_linear_system(self):
         func_vals = np.array(self.function_values).transpose()
         self.approx_solutions = np.linalg.solve(self.matrix, func_vals)
